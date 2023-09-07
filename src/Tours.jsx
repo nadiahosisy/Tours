e3415c555699ed9901903fc848dbfb16f0a45b8c
Your Name <youremail@example.com>
2023-09-07 11:44:12 +0300

    iterate over tour inside the component Tours

--- src/Tours.jsx
+++ src/Tours.jsx
@@ -1,9 +1,15 @@
+import Tour from "./Tour";
 const Tours = ({ tours }) => {
   return (
     <section>
       <div className="title">
         <h2>our tours</h2>
         <div className="title-underline"></div>
+      </div>
+      <div className="tours">
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} />;
+        })}
       </div>
     </section>
   );
