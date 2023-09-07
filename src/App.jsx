04a40c49d3c81f2b890664405b8aa278938eca09
Your Name <youremail@example.com>
2023-09-07 11:04:36 +0300

    add a fetchTours function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
-const url = 'https://course-api.com/react-tours-project';
+import { useEffect, useState } from "react";
+
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fechTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      console.log(tours);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fechTours();
+  }, []);
   return <h2>Tours Starter</h2>;
 };
 export default App;
