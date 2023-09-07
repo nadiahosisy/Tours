7e499411c90a47799e973bd05731d831dab52b83
Your Name <youremail@example.com>
2023-09-07 11:36:56 +0300

    add another div and header inside Tours component

--- src/Tours.jsx
+++ src/Tours.jsx
@@ -3,6 +3,7 @@ const Tours = ({ tours }) => {
     <section>
       <div className="title">
         <h2>our tours</h2>
+        <div className="title-underline"></div>
       </div>
     </section>
   );
