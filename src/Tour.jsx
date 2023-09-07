e04480a83af5059dbc3683849dcc35eb58569c82
Your Name <youremail@example.com>
2023-09-07 11:55:13 +0300

    add span and className inside the span i add the price inside Tour component

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -2,6 +2,7 @@ const Tour = ({ id, image, info, name, price }) => {
   return (
     <article className="single-tour">
       <img src={image} alt={name} className="img" />
+      <span className="tour-price">${price}</span>
     </article>
   );
 };
